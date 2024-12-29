18187ea87dd5872a07777cdef6bcc8469bea9c8e
timvisee <tim@visee.me>
2024-12-29 15:41:55 +0100

    Implement fruit pickup in snake, add manual controls

--- stories/Snake.gd
+++ stories/Snake.gd
@@ -11,14 +11,16 @@ var dir = 0;
 
 var blink_dead = 0;
 var is_done = false;
+var auto = true;
 
-var fruit = Vector2(1, 1);
+var fruit = Vector2.ZERO;
 
 
 func _start():
 	dir = 0;
 	enlarge = 2;
 	tail = [Vector2(int(width / 2), int(height / 2))];
+	fruit = Vector2(randi_range(0, width), randi_range(0, height));
 	wipe();
 
 
@@ -35,14 +37,24 @@ func _frame(frame):
 			draw(frame);
 		return;
 	
-	if (frame + 1) % 20 == 0:
-		enlarge += 1;
-	
-	if randf() <= 0.08:
-		if randf() <= 0.5:
-			dir = posmod(dir - 1, 4);
-		else:
-			dir = (dir + 1) % 4;
+	if auto:
+		if (frame + 1) % 20 == 0:
+			enlarge += 1;
+		
+		if randf() <= 0.08:
+			if randf() <= 0.5:
+				dir = posmod(dir - 1, 4);
+			else:
+				dir = (dir + 1) % 4;
+	else:
+		if Input.is_action_pressed("ui_up") && dir != 2:
+			dir = 0;
+		if Input.is_action_pressed("ui_right") && dir != 3:
+			dir = 1;
+		if Input.is_action_pressed("ui_down") && dir != 0:
+			dir = 2;
+		if Input.is_action_pressed("ui_left") && dir != 1:
+			dir = 3;
 	
 	var front = tail[0];
 	var next = front;
@@ -58,6 +70,9 @@ func _frame(frame):
 	if is_collision(next):
 		blink_dead = 10;
 		return;
+	if next == fruit:
+		enlarge += 3;
+		fruit = Vector2(randi_range(0, width), randi_range(0, height));
 	tail.push_front(next);
 	
 	if enlarge == 0:
@@ -85,10 +100,10 @@ func is_collision(pos: Vector2) -> bool:
 
 
 func draw(frame):
+	grid.set_dot(fruit.x, fruit.y, (int(frame / 4) % 2 == 0))
 	for pos in tail:
 		grid.set_dot(pos.x, pos.y, true);
-		
-	grid.set_dot(fruit.x, fruit.y, (int(frame / 4) % 2 == 0))
+
 
 func wipe():
 	for x in range(width):
