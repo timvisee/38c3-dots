5bbb48040b6dc979c07eb72204d7c6ffac7c4b54
timvisee <tim@visee.me>
2024-12-29 02:02:07 +0100

    Control grid position with arrows

--- scripts/Grid.gd
+++ scripts/Grid.gd
@@ -5,7 +5,8 @@ extends GridContainer
 @export var WIDTH: int = 5;
 @export var HEIGHT: int = 5;
 
-const prefab_dot = preload("res://prefabs/Dot.tscn")
+const PREFAB_DOT = preload("res://prefabs/Dot.tscn");
+const OFFSET_MOVE = 5.0;
 
 var padding = 10.0;
 var offset = Vector2.ZERO;
@@ -19,9 +20,9 @@ func _ready():
 	
 	# Add dots as configured
 	for i in WIDTH * HEIGHT:
-		var dot = prefab_dot.instantiate();
+		var dot = PREFAB_DOT.instantiate();
 		self.add_child(dot);
-		
+	
 	self.relayout();
 	
 
@@ -32,8 +33,22 @@ func _input(event):
 	if event.is_action_pressed("padding_min"):
 		self.padding -= 1.0;
 		self.relayout();
+		
+	if event.is_action("ui_up"):
+		self.offset += OFFSET_MOVE * Vector2.UP;
+		self.relayout();
+	if event.is_action("ui_down"):
+		self.offset += OFFSET_MOVE * Vector2.DOWN;
+		self.relayout();
+	if event.is_action("ui_left"):
+		self.offset += OFFSET_MOVE * Vector2.LEFT;
+		self.relayout();
+	if event.is_action("ui_right"):
+		self.offset += OFFSET_MOVE * Vector2.RIGHT;
+		self.relayout();
 
 
 func relayout():
 	self.add_theme_constant_override("h_separation", self.padding);
 	self.add_theme_constant_override("v_separation", self.padding);
+	self.position = self.offset;
